96b765d3f3ab7217db4424a4050eb8397ddd58ff
TomasVoelklein <124761351+TomasVoelklein@users.noreply.github.com>
2023-02-27 18:38:13 -0300

    Add files via upload

---
+++ css/nav.js
@@ -0,0 +1,8 @@
+// Obtener el botón por su ID
+const btnInicio = document.getElementById("btnInicio");
+
+// Agregar un evento de escucha de clic al botón
+btnInicio.addEventListener("click", function () {
+  // Redirigir al usuario a la página de inicio
+  window.location.href = "index.html";
+});
---
+++ scripts/funciones.js
@@ -0,0 +1,12 @@
+console.log ("hola mundo")
+
+document.getElementById("boton-enviar").addEventListener("click", function () {
+    window.location.href = "mailto:destinatario@ejemplo.com";
+});
+document.getElementById("boton-wsp").addEventListener("click", function () {
+    window.location.href='https://www.shorturl.at/dgFZ0' ;
+}) ;
+document.getElementById("boton-git").addEventListener("click", function () {
+    window.location.href='https://github.com/TomasVoelklein?tab=repositories' ;
+})
+
